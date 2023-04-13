b465e34cce923574655ddab02445d3faff2eac4e
Lab1-14 <Lab1-14@LAP1-14>
2023-04-13 14:13:09 +0300

    Add project files.

---
+++ SparkySimp.DeltaTransformer/PropertyTransformation.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SparkySimp.DeltaTransformer
+{
+    public class PropertyTransformation : Transformation
+    {
+
+        public PropertyTransformation()
+        {
+            DeltaProperties = new Dictionary<string, double>();
+            TransformationTick += (_sender, _e) =>
+            {
+                // TODO: Implement the delta application here. Delta represents changes in the control's properties intended for summing. Use bindings.
+            };
+        }
+
+        /// <summary>
+        /// Represents the changes in the properties' values.
+        /// </summary>
+        public Dictionary<string, double> DeltaProperties { get; set; }
+
+        public override TimeSpan Time { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override TimeSpan Elapsed => throw new NotImplementedException();
+
+        public override event EventHandler<TransformationEventArgs> TransformationComplete;
+        public override event EventHandler<TransformationEventArgs> TransformationStart;
+        public override event EventHandler<TransformationEventArgs> TransformationTick;
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override void Resume()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Start(Control control)
+        {
+            TransformationStart(this, new TransformationEventArgs { 
+                Control = control,
+                CurrentTime = TimeSpan.Zero
+            });
+        }
+
+        public override void Suspend()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ SparkySimp.DeltaTransformer/Transformation.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SparkySimp.DeltaTransformer
+{
+    /// <summary>
+    /// Represents event arguments for transforming object.
+    /// </summary>
+    public class TransformationEventArgs : EventArgs
+    {
+        /// <summary>
+        /// The <see cref="Control"/> that was used in transfomation.
+        /// </summary>
+        public Control Control { get; set; }
+
+        /// <summary>
+        /// The current position of the transformation.
+        /// </summary>
+        public TimeSpan CurrentTime { get; set; }
+
+    }
+    /// <summary>
+    /// Represents the base class for all transformations.
+    /// </summary>
+    public abstract class Transformation
+    {
+        /// <summary>
+        /// Represents the amount of time required to transform a control.
+        /// </summary>
+        public abstract TimeSpan Time { get; set; }
+
+        /// <summary>
+        /// Fires when the transformation completes.
+        /// </summary>
+        public abstract event EventHandler<TransformationEventArgs> TransformationComplete;
+
+        /// <summary>
+        /// Fires when the transformation starts.
+        /// </summary>
+        public abstract event EventHandler<TransformationEventArgs> TransformationStart;
+
+        /// <summary>
+        /// Fires every time a transformation progresses.
+        /// </summary>
+        public abstract event EventHandler<TransformationEventArgs> TransformationTick;
+
+        /// <summary>
+        /// Starts the transformation.
+        /// </summary>
+        /// <param name="control">The <see cref="Control"/> to apply this transformation on.</param>
+        public abstract void Start(Control control);
+
+        /// <summary>
+        /// Suspends the transformation temporarily.
+        /// </summary>
+        public abstract void Suspend();
+
+        /// <summary>
+        /// Resumes a suspended transformation.
+        /// </summary>
+        public abstract void Resume();
+
+        /// <summary>
+        /// Represents the time passed since the transforon has started.
+        /// </summary>
+        public abstract TimeSpan Elapsed { get; }
+    }
+}
